d27f8112dc3ad718759b8d4a8e757db581c768bf
Genouka <142009020+Genouka@users.noreply.github.com>
2026-01-02 00:44:24 +0800

    引入QiuUTMTv3的Skia版本UTMTLib

--- UndertaleModToolAvalonia/Core/ImageCache.cs
+++ UndertaleModToolAvalonia/Core/ImageCache.cs
@@ -25,26 +25,26 @@ public class ImageCache
         // Faster shortcut
         if (gmImage.Format == GMImage.ImageFormat.Png)
         {
-            return SKImage.FromEncodedData(gmImage.GetData());
+            return gmImage.GetSkiaImage();
         }
-
-        byte[] data = gmImage.ConvertToRawBgra().GetData();
-        GCHandle gcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
-
-        SKBitmap bitmap = new();
-
-        SKImageInfo info = new(gmImage.Width, gmImage.Height, SKColorType.Bgra8888, SKAlphaType.Unpremul);
-        SKPixmap pixmap = new(info, gcHandle.AddrOfPinnedObject(), info.RowBytes);
-        SKImage? image = SKImage.FromPixels(pixmap, delegate
-        { gcHandle.Free(); });
-
-        if (image is null)
-        {
-            gcHandle.Free();
-            throw new Exception("Could not create image");
-        }
-
-        return image;
+        return gmImage.ConvertToRawBgra().GetSkiaImage();
+        // byte[] data = gmImage.ConvertToRawBgra().GetData();
+        // GCHandle gcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+        //
+        // SKBitmap bitmap = new();
+        //
+        // SKImageInfo info = new(gmImage.Width, gmImage.Height, SKColorType.Bgra8888, SKAlphaType.Unpremul);
+        // SKPixmap pixmap = new(info, gcHandle.AddrOfPinnedObject(), info.RowBytes);
+        // SKImage? image = SKImage.FromPixels(pixmap, delegate
+        // { gcHandle.Free(); });
+        //
+        // if (image is null)
+        // {
+        //     gcHandle.Free();
+        //     throw new Exception("Could not create image");
+        // }
+        //
+        // return image;
     }
 
     public SKImage GetCachedImageFromGMImage(GMImage gmImage)
